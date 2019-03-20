8a269c548c2810e6881278fb00f5c93fd77bb8a9
Ethan <N/A>
2019-03-19 17:12:24 -0700

    Commit 9 - created loops.js file

---
+++ loop-questions/app.js
@@ -0,0 +1,17 @@
+
+console.log("loop 1");
+for(let i = 0; i<100;i++){console.log(i);}
+console.log("loop 2");
+for(let i = 99; i>=0;i--){console.log(i);}
+console.log("loop 3");
+for(let i = 298; i>=1;i=i-3){console.log(i);}
+console.log("loop 4");
+for(let i = 0; i<=98;i=i+2){console.log(i);}
+console.log("loop 5");
+for(let i = 97; i>0;i=i-2){console.log(i);}
+console.log("loop 6");
+for(let i = 49; i<=72;i++){console.log(i);}
+console.log("loop 7");
+for(let i = 81; i>=26;i--){console.log(i);}
+console.log("loop 8");
+for(let i = 1; i*3<=91;i++){console.log(i*3);}
